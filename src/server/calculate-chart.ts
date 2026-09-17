76c1629a3e2cfcaa4d736295ee289f72c2ee9d8f
thaletto <krlaxman03@gmail.com>
2026-09-17 23:25:42 +0530

    fix: Default AstroParams

--- src/server/calculate-chart.ts
+++ src/server/calculate-chart.ts
@@ -53,7 +53,10 @@ export class CalculateChartError extends Data.TaggedError("CalculateChartError")
   readonly message: string;
 }> {}
 
-const AstroLayer = Layer.mergeAll(AstroParams.DefaultAstroParams, Swisseph.SwissephLayer);
+const AstroLayer = Layer.mergeAll(
+  AstroParams.layer({ ayanamsa: "Lahiri", houseSystem: "WholeSign" }),
+  Swisseph.SwissephLayer,
+);
 
 const KpAstroLayer = Layer.mergeAll(AstroParams.DefaultAstroParams, Swisseph.SwissephLayer);
 
